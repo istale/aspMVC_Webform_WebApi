f4ce2862984724fbcfcb118d18db77c21f724a3c
istale <iscarryon@gmail.com>
2014-04-03 20:42:40 +0800

    initial commit

---
+++ MVC_WebForm_WebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_WebForm_WebApi.Startup))]
+namespace MVC_WebForm_WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
